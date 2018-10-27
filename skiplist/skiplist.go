2452c1bbfd8b889c4831a45cef5d52221c06ae78
zhaoch <358638311@qq.com>
2018-10-27 15:02:16 +0800

    跳表时间复杂度

--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -1,294 +1,126 @@
 package skiplist
 
+
 import (
-	"fmt"
 	"math/rand"
+	"fmt"
 )
 
-const SKIPLIST_MAXLEVEL = 32
+const SKIPLIST_MAXLEVEL = 8
 
-// 跳表
+type Node struct {
+	Forward  []Node
+	Value interface{}
+}
+
+func NewNode(v interface{},level int) *Node {
+	return &Node{
+		Value: v,
+		Forward: make([]Node, level),
+	}
+}
+
 type SkipList struct {
-	header   *skipListNode
-	tail     *skipListNode
-	node_map map[interface{}]*skipListNode
-	level    int
-	Less     func(interface{}, interface{}) bool
+	Header *Node
+	Level  int
 }
-
-type skipListLevel struct {
-	forward *skipListNode
-	span    int
-}
-
-type skipListNode struct {
-	key      interface{}
-	data     interface{}
-	backward *skipListNode
-	levels   []skipListLevel
-}
-
-/**************************public**************************/
 
 func NewSkipList() *SkipList {
-	sl := &SkipList{
-		level:    1,
-		node_map: make(map[interface{}]*skipListNode),
-		header:   newSkipListNode(SKIPLIST_MAXLEVEL, 0, 0),
-	}
-
-	sl.header.backward = nil
-	sl.tail = nil
-
-	return sl
+	return &SkipList{Level: 1, Header: NewNode(0,SKIPLIST_MAXLEVEL)}
 }
 
-// 获取Level
-func (s *SkipList) Level() int {
-	return s.level
-}
+func (skipList *SkipList) Insert(key int) {
 
-// 获取排行榜数据数量
-func (s *SkipList) Length() int {
-	return len(s.node_map)
-}
+	update := make(map[int]*Node)
+	node := skipList.Header
 
-// 根据Key设置数据
-func (s *SkipList) Set(key interface{}, data interface{}) error {
-	if e := s.Delete(key); e != nil {
-		return e
-	}
-
-	var update [SKIPLIST_MAXLEVEL]*skipListNode
-	var rank [SKIPLIST_MAXLEVEL]int
-	var node *skipListNode
-
-	node = s.header
-
-	for i := s.level - 1; i >= 0; i-- {
-		if i == s.level-1 {
-			rank[i] = 0
-		} else {
-			rank[i] = rank[i+1]
-		}
-
-		for node.levels[i].forward != nil && s.Less(node.levels[i].forward.data, data) {
-			rank[i] += node.levels[i].span
-			node = node.levels[i].forward
+	for i := skipList.Level - 1; i >= 0; i-- {
+		for {
+			if node.Forward[i].Value != nil && node.Forward[i].Value.(int) < key {
+				node = &node.Forward[i]
+			}else{
+				break;
+			}
 		}
 		update[i] = node
 	}
 
-	var level = randomLevel()
-	if level > s.level {
-		for i := s.level; i < level; i++ {
-			rank[i] = 0
-			update[i] = s.header
-			update[i].levels[i].span = s.Length()
+	level := skipList.RandomLevel()
+	if level > skipList.Level {
+		for i := skipList.Level; i < level; i++ {
+			update[i] = skipList.Header
 		}
-		s.level = level
+		skipList.Level = level
 	}
 
-	node = newSkipListNode(level, key, data)
-	s.node_map[key] = node
+	newNode := NewNode(key,level)
 
 	for i := 0; i < level; i++ {
-		node.levels[i].forward = update[i].levels[i].forward
-		update[i].levels[i].forward = node
-
-		node.levels[i].span = update[i].levels[i].span - (rank[0] - rank[i])
-		update[i].levels[i].span = (rank[0] - rank[i]) + 1
+		newNode.Forward[i] = update[i].Forward[i]
+		update[i].Forward[i] = *newNode
 	}
 
-	for i := level; i < s.level; i++ {
-		update[i].levels[i].span++
+}
+
+func (skipList *SkipList) Remove(key int) {
+
+	update := make(map[int]*Node)
+	node := skipList.Header
+	for i := skipList.Level - 1; i >= 0; i-- {
+		for {
+			if node.Forward[i].Value == nil {
+				break
+			}
+			if node.Forward[i].Value.(int) == key {
+				update[i] = node
+				break
+			}
+			if  node.Forward[i].Value.(int) < key {
+				node = &node.Forward[i]
+				continue
+			}else{
+				break
+			}
+		}
 	}
 
-	if update[0] == s.header {
-		node.backward = nil
-	} else {
-		node.backward = update[0]
+	for i,v := range update {
+		if v == skipList.Header {
+			skipList.Level --
+		}
+		v.Forward[i] = v.Forward[i].Forward[i]
 	}
+}
 
-	if node.levels[0].forward != nil {
-		node.levels[0].forward.backward = node
-	} else {
-		s.tail = node
+func (skipList *SkipList) Search(key int) *Node {
+
+	node := skipList.Header
+	for i := skipList.Level - 1; i >= 0; i-- {
+		for {
+			if node.Forward[i].Value == nil {
+				break
+			}
+
+			if node.Forward[i].Value.(int) == key {
+				return &node.Forward[i]
+			}
+
+			if  node.Forward[i].Value.(int) < key {
+				node = &node.Forward[i]
+				continue
+			} else {
+				break
+			}
+		}
 	}
-
 	return nil
 }
 
-// 根据Key获取数据
-func (s *SkipList) Get(key interface{}) interface{} {
-	if node := s.getNode(key); node != nil {
-		return node.data
-	}
-
-	return nil
-}
-
-// 根据Key删除数据
-func (s *SkipList) Delete(key interface{}) error {
-	if node := s.getNode(key); node != nil {
-		return s.deleteByNode(node)
-	}
-
-	return nil
-}
-
-// 根据Key判断是否存在
-func (s *SkipList) Exist(key interface{}) bool {
-	return s.Get(key) != nil
-}
-
-// 根据Key获取排名
-func (s *SkipList) GetRank(key interface{}) int {
-	if node := s.getNode(key); node != nil {
-		return s.GetRankByData(node.data)
-	}
-
-	return 0
-}
-
-// 根据数据获取排名
-func (s *SkipList) GetRankByData(data interface{}) int {
-	var node *skipListNode
-	var rank int = 1
-
-	node = s.header
-	for i := s.level - 1; i >= 0; i-- {
-		for node.levels[i].forward != nil && s.Less(node.levels[i].forward.data, data) {
-			rank += node.levels[i].span
-			node = node.levels[i].forward
-		}
-	}
-
-	node = node.levels[0].forward
-	if node != nil && data == node.data {
-		return rank
-	}
-
-	return 0
-}
-
-// 根据排名获取数据
-func (s *SkipList) GetDataByRank(rank int) interface{} {
-	if rank == 0 || rank > s.Length() {
-		return nil
-	}
-
-	var node *skipListNode
-	var traversed int = 0
-
-	node = s.header
-	for i := s.level - 1; i >= 0; i-- {
-		for node.levels[i].forward != nil && (traversed+node.levels[i].span) <= rank {
-			traversed += node.levels[i].span
-			node = node.levels[i].forward
-		}
-
-		if traversed == rank {
-			return node.data
-		}
-	}
-
-	return nil
-}
-
-// 获取排名前多少名数据
-func (s *SkipList) Top(number int) []interface{} {
-	var top_list = make([]interface{}, number)
-	var index int
-
-	node := s.header.levels[0].forward
-	for node != nil && index < number {
-		top_list[index] = node.data
-		index++
-		node = node.levels[0].forward
-	}
-
-	return top_list
-}
-
-// 获取所有数据
-func (s *SkipList) All() []interface{} {
-	var top_list = []interface{}{}
-	var index int
-
-	node := s.header.levels[0].forward
-	for node != nil {
-		top_list = append(top_list, node.data)
-		index++
-		node = node.levels[0].forward
-	}
-
-	return top_list
-}
-
-// 获取排名范围内的数据
-func (s *SkipList) Range(min, max int) []interface{} {
-	var top_list = make([]interface{}, max-min)
-	var index int
-
-	node := s.header.levels[0].forward
-	for node != nil {
-		if index >= max {
-			break
-		}
-
-		if index >= min {
-			top_list[index-min] = node.data
-		}
-
-		node = node.levels[0].forward
-		index++
-	}
-
-	return top_list
-}
-
-// 获取排名后多少名数据
-func (s *SkipList) Bottom(number int) []interface{} {
-	var bottom_list = make([]interface{}, number)
-	var index int
-
-	node := s.tail
-	for node != nil && index < number {
-		bottom_list[index] = node.data
-		index++
-		node = node.backward
-	}
-
-	return bottom_list
-}
-
-// 清空排行榜
-func (s *SkipList) Clear() {
-	s.header = newSkipListNode(SKIPLIST_MAXLEVEL, 0, 0)
-	s.tail = nil
-	s.node_map = make(map[interface{}]*skipListNode)
-	s.level = 1
-}
-
-/**************************private**************************/
-
-func newSkipListNode(level int, key interface{}, data interface{}) *skipListNode {
-	sn := &skipListNode{
-		key:    key,
-		data:   data,
-		levels: make([]skipListLevel, level),
-	}
-	return sn
-}
-
-// 随机节点高度
-func randomLevel() int {
+func (skipList *SkipList) RandomLevel() int {
 	var level int = 1
 	for {
 		if rand.Intn(2) == 1 {
 			level++
-
 			if level >= SKIPLIST_MAXLEVEL {
 				break
 			}
@@ -297,66 +129,25 @@ func randomLevel() int {
 			break
 		}
 	}
-
 	return level
 }
 
-// 根据数据删除节点
-func (s *SkipList) deleteByData(data interface{}) error {
-	var update = make([]*skipListNode, SKIPLIST_MAXLEVEL)
-	var node *skipListNode
+func (skipList *SkipList) PrintSkipList() {
 
-	node = s.header
-	for i := s.level - 1; i >= 0; i-- {
-		for node.levels[i].forward != nil && s.Less(node.levels[i].forward.data, data) {
-			node = node.levels[i].forward
+	for i := SKIPLIST_MAXLEVEL - 1; i >= 0; i-- {
+
+		fmt.Println("level:", i)
+		node := skipList.Header.Forward[i]
+		for {
+			if node.Value != nil {
+				fmt.Printf("%d ", node.Value.(int))
+				node = node.Forward[i]
+			}else{
+				break
+			}
 		}
-		update[i] = node
+		fmt.Println("\n--------------------------------------------------------")
 	}
 
-	node = node.levels[0].forward
-	if data == node.data {
-		s.deleteNode(node, update)
-		return nil
-	}
-
-	return fmt.Errorf("deleteByData not found data : %v", data)
-}
-
-// 根据节点删除
-func (s *SkipList) deleteByNode(node *skipListNode) error {
-	if node == nil {
-		return nil
-	}
-
-	return s.deleteByData(node.data)
-}
-
-// 删除节点
-func (s *SkipList) deleteNode(x *skipListNode, update []*skipListNode) {
-	for i := 0; i < s.level; i++ {
-		if update[i].levels[i].forward == x {
-			update[i].levels[i].span += x.levels[i].span - 1
-			update[i].levels[i].forward = x.levels[i].forward
-		} else {
-			update[i].levels[i].span -= 1
-		}
-	}
-
-	if x.levels[0].forward != nil {
-		x.levels[0].forward.backward = x.backward
-	} else {
-		s.tail = x.backward
-	}
-
-	for s.level > 1 && s.header.levels[s.level-1].forward == nil {
-		s.level--
-	}
-
-	delete(s.node_map, x.key)
-}
-
-// 获取节点
-func (s *SkipList) getNode(key interface{}) *skipListNode {
-	return s.node_map[key]
+	fmt.Println("Current MaxLevel:", skipList.Level)
 }
